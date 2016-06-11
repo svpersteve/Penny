d186976ca73ec586a78259b8c9b3472338737935
Steve Brewer <steve.brewer@futurelearn.com>
2016-06-11 11:11:57 +0100

    Transfer initial files from workspace

---
+++ app.js
@@ -0,0 +1,7 @@
+// Goal: Make a program that turns all images in the img folder into gallery items on the page, so all the client needs to do is upload an image file to the folder and it's added to the gallery. 
+
+//Images may need to be resized as clients may upload enormous images
+
+// Create a system that allows folders in /img to define gallery headings, e.g. a Nature folder puts all images contained into a Nature gallery
+
+// Use IDs and a connected nav menu for users to navigate around the site. 
